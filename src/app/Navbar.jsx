42687e43524c6470700d01386c054015480e5fd2
Andrew <andrewheekin@gmail.com>
2023-12-12 08:45:44 -0500

    github icon

--- src/app/Navbar.jsx
+++ src/app/Navbar.jsx
@@ -1,7 +1,8 @@
-import { AppBar, Toolbar, Typography, Button } from "@mui/material";
-import { UserButton, auth, currentUser, SignInButton } from "@clerk/nextjs";
-import Image from "next/image";
-import Link from "next/link";
+import { AppBar, Toolbar, Typography, Button, IconButton } from '@mui/material';
+import { UserButton, auth, currentUser, SignInButton } from '@clerk/nextjs';
+import Image from 'next/image';
+import Link from 'next/link';
+import GitHubIcon from '@mui/icons-material/GitHub';
 
 export default async function Navbar() {
   /**
@@ -19,42 +20,47 @@ export default async function Navbar() {
   // Use `user` to render user details or create UI element
 
   return (
-    <AppBar position="static" style={{ backgroundColor: "black", border: "3px solid black" }}>
-      <Toolbar style={{ justifyContent: "space-between" }}>
-        <div style={{ display: "flex", flexDirection: "row", alignItems: "flex-start" }}>
+    <AppBar position="static" style={{ backgroundColor: 'black', border: '3px solid black' }}>
+      <Toolbar style={{ justifyContent: 'space-between' }}>
+        <div style={{ display: 'flex', flexDirection: 'row', alignItems: 'flex-start' }}>
           <Image
             src="https://media.tenor.com/dhfraztxBo8AAAAj/globe-joypixels.gif"
             alt="Globe"
             width={40}
             height={40}
             unoptimized
-            style={{ margin: "0 8px 0 0" }}
+            style={{ margin: '0 8px 0 0' }}
           />
           <Link href="/">
             <Typography
               variant="h6"
               component="div"
-              style={{ color: "white", fontFamily: "monospace", display: "flex", flexDirection: "column" }}
+              style={{ color: 'white', fontFamily: 'monospace', display: 'flex', flexDirection: 'column' }}
             >
-              <span style={{ lineHeight: "1.2" }}>GeoNotes</span>
-              <span style={{ fontSize: "0.7rem" }}>Learn the world</span>
+              <span style={{ lineHeight: '1.2' }}>GeoNotes</span>
+              <span style={{ fontSize: '0.7rem' }}>Learn the world</span>
               {/* <span style={{ fontSize: "0.7rem" }}>Launched Nov 2023 🎉</span> */}
             </Typography>
           </Link>
         </div>
-        <div style={{ display: "flex" }}>
+        <div style={{ display: 'flex' }}>
+          <Link href="https://github.com/andrewheekin/geonotes.io">
+            <IconButton sx={{ color: 'white', '&:hover': { color: 'lightgray' } }}>
+              <GitHubIcon />
+            </IconButton>
+          </Link>
           <Link href="/about">
-            <Button color="inherit" style={{ fontFamily: "monospace" }}>
+            <Button color="inherit" style={{ fontFamily: 'monospace' }}>
               About
             </Button>
           </Link>
           <Link href="/submit">
-            <Button color="inherit" style={{ fontFamily: "monospace", letterSpacing: "-0.8px" }}>
+            <Button color="inherit" style={{ fontFamily: 'monospace', letterSpacing: '-0.8px' }}>
               Submit a GeoNote
             </Button>
           </Link>
           {userId ? (
-            <div style={{ margin: "0 0 0 10px" }}>
+            <div style={{ margin: '0 0 0 10px' }}>
               <UserButton afterSignOutUrl="/" />
             </div>
           ) : (
@@ -62,7 +68,7 @@ export default async function Navbar() {
               mode="modal"
               afterSignInUrl="/"
               afterSignUpUrl="/"
-              style={{ fontFamily: "monospace", fontSize: "0.9rem", letterSpacing: "-0.6px" }}
+              style={{ fontFamily: 'monospace', fontSize: '0.9rem', letterSpacing: '-0.6px' }}
             >
               SIGN IN
             </SignInButton>
