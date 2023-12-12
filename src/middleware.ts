03fef89de8cfe12671a21e4b65530ecf7b654285
Andrew <andrewheekin@gmail.com>
2023-12-12 09:02:24 -0500

    more public routes

--- src/middleware.ts
+++ src/middleware.ts
@@ -5,7 +5,7 @@ import { authMiddleware } from "@clerk/nextjs";
 // See https://clerk.com/docs/references/nextjs/auth-middleware for more information about configuring your Middleware
 export default authMiddleware({
   // "/" will be accessible to all users
-  publicRoutes: ["/"],
+  publicRoutes: ["/", "/submit", "/about"],
 });
 
 export const config = {
