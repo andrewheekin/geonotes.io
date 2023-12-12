d87d97a493c3720d4908114677d02f75582f1075
Andrew <andrewheekin@gmail.com>
2023-12-12 09:16:51 -0500

    open source banner

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,17 +1,42 @@
 // src/app/page.js
 
-import { Suspense } from "react";
-import { unstable_noStore as noStore } from "next/cache";
-import GeoNotesContainer from "./GeoNotesContainer";
-import { supabase } from "./SupabaseClient";
+import { Suspense } from 'react';
+import { Typography } from '@mui/material';
+import { unstable_noStore as noStore } from 'next/cache';
+import GeoNotesContainer from './GeoNotesContainer';
+import { supabase } from './SupabaseClient';
 
 export default async function Home() {
   noStore();
-  const { data: geoNotes } = await supabase.from("geonote").select("*");
+  const { data: geoNotes } = await supabase.from('geonote').select('*');
 
   return (
-    <Suspense>
-      <GeoNotesContainer geoNotes={geoNotes} />
-    </Suspense>
+    <>
+      <div style={{ display: 'flex', flexDirection: 'row', alignItems: 'flex-start', justifyContent: 'center' }}>
+        <Typography
+          variant="body2"
+          color="text.secondary"
+          style={{
+            fontFamily: 'monospace',
+            color: 'black',
+            margin: '0px 0 10px 0',
+            background: '#c6fbc6',
+            padding: '10px',
+            borderRadius: '10px',
+            width: '50%',
+            justifyContent: 'center',
+          }}
+        >
+          GeoNotes.io is looking for open source contributors! If you are interested in helping build the site,
+          pick up an issue at <a href="https://github.com/andrewheekin/geonotes.io/issues" style={{
+            color: 'navy',
+            textDecoration: 'underline',
+          }}>GitHub</a>!
+        </Typography>
+      </div>
+      <Suspense>
+        <GeoNotesContainer geoNotes={geoNotes} />
+      </Suspense>
+    </>
   );
 }
