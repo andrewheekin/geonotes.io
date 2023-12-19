83f34eb22fce983407ea79f06f22371456ae7894
Andrew <andrewheekin@gmail.com>
2023-12-19 18:27:38 -0500

    fetchGeoNote filtering from country and category paramas

--- app/_lib/actions.js
+++ app/_lib/actions.js
@@ -27,107 +27,30 @@ export async function fetchGeoNotes({ searchParams }) {
   const cookieStore = cookies();
   const supabase = createServerActionClient({ cookies: () => cookieStore });
 
-  /**
-   * Countries filtering
-   *
-   */
-  
-  if (Object.prototype.hasOwnProperty.call(searchParams, 'countries') && searchParams.countries.length > 0) {
-    const countries = searchParams.countries.split(',');
-    console.log('countries', countries);
+  const hasCountries =
+    Object.prototype.hasOwnProperty.call(searchParams, 'countries') && searchParams.countries.length > 0;
+  const hasCategories =
+    Object.prototype.hasOwnProperty.call(searchParams, 'categories') && searchParams.categories.length > 0;
+  const hasAuthor = Object.prototype.hasOwnProperty.call(searchParams, 'author') && searchParams.author.length > 0;
 
-    const { data, error } = await supabase.from('geonote').select('*').in('country', countries);
+  let countries = [];
+  let categories = [];
+  let author = '';
 
-    if (error) {
-      console.error('Database Error: ', error);
-      return {
-        message: 'Database Error: Failed to Fetch GeoNotes.',
-      };
-    }
-
-    return data;
+  if (hasCountries) {
+    countries = searchParams.countries.split(',');
+  }
+  if (hasCategories) {
+    categories = searchParams.categories.split(',');
+  }
+  if (hasAuthor) {
+    author = searchParams.author;
   }
 
-  /**
-   * Categories filtering
-   */
-  if (Object.prototype.hasOwnProperty.call(searchParams, 'categories') && searchParams.categories.length > 0) {
-    const categories = searchParams.categories.split(',');
-
-    console.log('categories', categories);
-    // Fetch geonote IDs from the mapping table. Rows must match all categories.
-    const { data: mappingData, error: mappingError } = await supabase
-      .from('geonote_category_mapping')
-      .select('*')
-      .in('category_name', categories);
-
-    console.log('mappingData', mappingData);
-
-    // Here is an example of mappingData:
-    // mappingData [
-    //   { geonote_id: 36, category_name: 'Utility Poles' },
-    //   { geonote_id: 39, category_name: 'Utility Poles' },
-    //   { geonote_id: 18, category_name: 'Utility Poles' },
-    //   { geonote_id: 19, category_name: 'Utility Poles' },
-    //   { geonote_id: 20, category_name: 'Utility Poles' },
-    //   { geonote_id: 36, category_name: 'Bollards' },
-    //   { geonote_id: 23, category_name: 'Bollards' },
-    //   { geonote_id: 24, category_name: 'Bollards' },
-    //   { geonote_id: 25, category_name: 'Utility Poles' },
-    //   { geonote_id: 32, category_name: 'Bollards' },
-    //   { geonote_id: 47, category_name: 'Bollards' },
-    //   { geonote_id: 49, category_name: 'Bollards' },
-    //   { geonote_id: 50, category_name: 'Bollards' },
-    //   { geonote_id: 51, category_name: 'Bollards' },
-    //   { geonote_id: 52, category_name: 'Bollards' },
-    //   { geonote_id: 3, category_name: 'Utility Poles' }
-    // ]
-
-    // Please only return the geonote ID's that match ALL the categories. For example, geonote_id 36 matches both Utility Poles and Bollards, but geonote_id 25 only matches Utility Poles. So, only return geonote_id 36.
-
-    // Group the mappingData by geonote_id
-    const geonoteCategoryMap = mappingData.reduce((acc, item) => {
-      if (!acc[item.geonote_id]) {
-        acc[item.geonote_id] = new Set();
-      }
-      acc[item.geonote_id].add(item.category_name);
-      return acc;
-    }, {});
-
-    // Filter out geonote_id that match all categories
-    const filteredGeonoteIds = Object.entries(geonoteCategoryMap)
-      .filter(([_, categorySet]) => categories.every((cat) => categorySet.has(cat)))
-      .map(([geonoteId, _]) => geonoteId);
-
-    // Use filteredGeonoteIds instead of mappingData for further operations
-
-    if (mappingError) {
-      console.error('Database Error: ', mappingError);
-      return {
-        message: 'Database Error: Failed to Fetch GeoNote Categories.',
-      };
-    }
-
-    // Fetch the geonotes that match these IDs (aka that match all categories)
-    const { data, error } = await supabase.from('geonote').select('*').in('id', filteredGeonoteIds);
-
-    if (error) {
-      console.error('Database Error: ', error);
-      return {
-        message: 'Database Error: Failed to Fetch GeoNotes.',
-      };
-    }
-
-    return data;
-  }
-
-  /**
-   * Author filtering
-   *
-   */
-  if (Object.prototype.hasOwnProperty.call(searchParams, 'author') && searchParams.author.length > 0) {
-    const author = searchParams.author;
-
+  if (hasAuthor) {
+    /**
+     * Case where author is provided
+     */
     const { data, error } = await supabase.from('geonote').select('*').eq('author_id', author);
 
     if (error) {
@@ -140,16 +63,128 @@ export async function fetchGeoNotes({ searchParams }) {
     return data;
   }
 
-  const { data, error } = await supabase.from('geonote').select('*').limit(NUM_INITIAL_GEONOTES);
+  if (hasCountries && hasCategories) {
+    /**
+     * Case where both countries and categories are provided
+     */
+    // Fetch geonote IDs from the mapping table that match all categories
+    const { data: mappingData, error: mappingError } = await supabase
+      .from('geonote_category_mapping')
+      .select('*')
+      .in('category_name', categories);
 
-  if (error) {
-    console.error('Database Error: ', error);
-    return {
-      message: 'Database Error: Failed to Fetch GeoNotes.',
-    };
+    if (mappingError) {
+      console.error('Database Error: ', mappingError);
+      return {
+        message: 'Database Error: Failed to Fetch GeoNote Categories.',
+      };
+    }
+
+    // Group the mappingData by geonote_id
+    const geonoteCategoryMap = mappingData.reduce((acc, item) => {
+      if (!acc[item.geonote_id]) {
+        acc[item.geonote_id] = new Set();
+      }
+      acc[item.geonote_id].add(item.category_name);
+      return acc;
+    }, {});
+
+    const filteredGeonoteIds = Object.entries(geonoteCategoryMap)
+      .filter(([_, categorySet]) => categories.every((cat) => categorySet.has(cat)))
+      .map(([geonoteId, _]) => geonoteId);
+
+    // Fetch the geonotes that match these IDs and are in the selected countries
+    const { data, error } = await supabase
+      .from('geonote')
+      .select('*')
+      .in('id', filteredGeonoteIds)
+      .in('country', countries);
+
+    if (error) {
+      console.error('Database Error: ', error);
+      return {
+        message: 'Database Error: Failed to Fetch GeoNotes.',
+      };
+    }
+
+    return data;
+  } else if (hasCountries) {
+    /**
+     * Case where only countries are provided
+     */
+    // Only countries filter is provided
+    const { data: countriesData, error: countriesError } = await supabase
+      .from('geonote')
+      .select('*')
+      .in('country', countries);
+
+    if (countriesError) {
+      console.error('Database Error: ', countriesError);
+      return {
+        message: 'Database Error: Failed to Fetch GeoNotes.',
+      };
+    }
+
+    return countriesData;
+  } else if (hasCategories) {
+    /**
+     * Case where only categories are provided
+     */
+    const { data: mappingData, error: mappingError } = await supabase
+      .from('geonote_category_mapping')
+      .select('*')
+      .in('category_name', categories);
+
+    if (mappingError) {
+      console.error('Database Error: ', mappingError);
+      return {
+        message: 'Database Error: Failed to Fetch GeoNote Categories.',
+      };
+    }
+
+    // Group the mappingData by geonote_id
+    const geonoteCategoryMap = mappingData.reduce((acc, item) => {
+      if (!acc[item.geonote_id]) {
+        acc[item.geonote_id] = new Set();
+      }
+      acc[item.geonote_id].add(item.category_name);
+      return acc;
+    }, {});
+
+    const filteredGeonoteIds = Object.entries(geonoteCategoryMap)
+      .filter(([_, categorySet]) => categories.every((cat) => categorySet.has(cat)))
+      .map(([geonoteId, _]) => geonoteId);
+
+    const { data: categoryData, error: categoryError } = await supabase
+      .from('geonote')
+      .select('*')
+      .in('id', filteredGeonoteIds);
+
+    if (categoryError) {
+      console.error('Database Error: ', categoryError);
+      return {
+        message: 'Database Error: Failed to Fetch GeoNotes.',
+      };
+    }
+
+    return categoryData;
+  } else {
+    /**
+     * Case where No filters provided, return default set of GeoNotes
+     *
+     */
+
+    const { data, error } = await supabase.from('geonote').select('*').limit(NUM_INITIAL_GEONOTES);
+
+    if (error) {
+      console.error('Database Error: ', error);
+      return {
+        message: 'Database Error: Failed to Fetch GeoNotes.',
+      };
+    }
+
+    return data;
   }
-
-  return data;
 }
 
 export async function createGeoNote(prevState, formData) {
