4024b3da3f90f271c61b1c35e41857e5bd93e10e
Andrew <andrewheekin@gmail.com>
2024-01-04 08:04:38 -0500

    mobile footer

--- app/_components/Footer.jsx
+++ app/_components/Footer.jsx
@@ -4,24 +4,24 @@ import GitHubIcon from '@mui/icons-material/GitHub'; // Import GitHubIcon
 
 export default function Footer() {
   return (
-    <footer className="shadow-none container mx-auto flex flex-wrap items-center justify-between px-4 py-6 md:py-12">
+    <footer className="shadow-none container mx-auto flex flex-wrap flex-col md:flex-row items-center justify-between px-4 py-6 md:py-12">
       <div className="flex items-center pt-4 md:pt-0">
         <Image src="/globe.png" alt="Globe" width={20} height={20} unoptimized className="mr-2" />
         <Link href="/" className="text-lg text-black font-bold tracking-tighter">
           GeoNotes
         </Link>
       </div>
-      <span className="text-black text-sm font-mono font-medium pb-4 md:pb-0">©2023 GeoNotes. All rights reserved.</span>
+      <span className="text-black text-sm font-medium pb-4 md:pb-0">©2023 GeoNotes. All rights reserved.</span>
 
       <div className="flex items-center space-x-12">
         <Link
           href="https://github.com/andrewheekin/geonotes.io"
-          className="flex text-black text-md font-mono font-medium hover:text-gray-500"
+          className="flex text-black text-md font-medium hover:text-gray-500"
         >
           <GitHubIcon />
           <div className="ml-1">GitHub</div>
         </Link>
-        <Link href="/privacy-policy" className="text-black hover:text-gray-500 font-mono font-medium">
+        <Link href="/privacy-policy" className="text-black hover:text-gray-500 font-medium">
           Privacy Policy
         </Link>
       </div>
