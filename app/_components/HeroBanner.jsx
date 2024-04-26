1ec90e537d3d8178ac6af6e002bd872cdbfccf13
LuggaPugga <LuggaPugga>
2024-04-26 15:51:40 +0200

    added dark-mode to HeroBanner

--- app/_components/HeroBanner.jsx
+++ app/_components/HeroBanner.jsx
@@ -44,7 +44,7 @@ export default function HeroBanner() {
   }
 
   // Conditional classes for fade-out effect
-  const bannerClasses = `relative w-full pt-4 pb-8 my-4 md:mb-10 bg-[rgba(0,0,0,0.1)] rounded-md transition-opacity duration-${FADE_DURATION} ${
+  const bannerClasses = `relative w-full pt-4 pb-8 my-4 md:mb-10 bg-[rgba(0,0,0,0.1)] dark:bg-[rgba(255,255,255,0.1)] rounded-md transition-opacity duration-${FADE_DURATION} ${
     isFadingOut ? 'opacity-0' : 'opacity-100'
   }`;
 
@@ -52,22 +52,22 @@ export default function HeroBanner() {
     <div className={bannerClasses}>
       <button
         onClick={handleClose}
-        className="absolute -top-2 -right-2 text-[rgba(0,0,0,0.7)] hover:text-[rgba(0,0,0,0.5)]"
+        className="absolute -top-2 -right-2 text-[rgba(0,0,0,0.7)] hover:text-[rgba(0,0,0,0.5)] dark:text-[rgba(255,255,255,0.7)] dark:hover:text-[rgba(255,255,255,0.5)]"
       >
-        <HighlightOffIcon className="bg-white rounded-2xl" />
+        <HighlightOffIcon className="bg-white dark:bg-black rounded-2xl" />
       </button>
       <div className="hidden sm:mb-6 sm:flex sm:justify-center">
-        <div className="relative rounded-full px-3 py-1 font-mono text-sm leading-6 text-gray-600 ring-1 ring-gray-900/10 hover:ring-gray-900/20">
+        <div className="relative rounded-full px-3 py-1 font-mono text-sm leading-6 text-gray-600 dark:text-gray-300 ring-1 ring-gray-900/10 dark:ring-gray-300/10 hover:ring-gray-900/20 dark:hover:ring-gray-300/20">
           Learn locations around the world with GeoNotes.{' '}
-          <Link href="/account" className="font-semibold text-cyan-600">
+          <Link href="/account" className="font-semibold text-cyan-600 dark:text-cyan-300">
             <span className="absolute inset-0" aria-hidden="true" />
             Learn now <span aria-hidden="true">&rarr;</span>
           </Link>
         </div>
       </div>
       <div className="text-center">
-        <div className="text-3xl font-mono font-bold tracking-tight text-gray-900">Welcome to GeoNotes</div>
-        <p className="mt-4 max-w-xl mx-auto font-mono text-md leading-8 text-gray-600">
+        <div className="text-3xl font-mono font-bold tracking-tight text-gray-900 dark:text-white">Welcome to GeoNotes</div>
+        <p className="mt-4 max-w-xl mx-auto font-mono text-md leading-8 text-gray-600 dark:text-gray-300">
           GeoNotes.io is the open source platform for community-contributed Street View notes
         </p>
       </div>
