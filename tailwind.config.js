4537a240cbf95d2078e78fcc81ecf278fe7c4ba0
LuggaPugga <LuggaPugga>
2024-04-26 15:45:42 +0200

    added dark-mode gradients to tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,6 +10,8 @@ module.exports = {
       backgroundImage: {
         'radial-gradient': 'radial-gradient(white, transparent)',
         'conic-gradient': 'conic-gradient(#87CEEB, #ADD8E6)',
+        'dark-radial-gradient': 'radial-gradient(black, transparent)',
+        'dark-conic-gradient': 'conic-gradient(#1e40af, #000000)',
       },
       blur: {
         '50': '50px',
