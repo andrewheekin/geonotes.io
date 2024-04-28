2f9face53b871804609b339781de9992527ecaa9
LuggaPugga <LuggaPugga>
2024-04-28 17:34:02 +0200

    added dark-mode to submit form

--- app/submit/page.jsx
+++ app/submit/page.jsx
@@ -3,7 +3,7 @@ import AddGeoNoteForm from './AddGeoNoteForm';
 export default function Submit() {
   return (
     <div className="max-w-2xl w-full py-2">
-      <h1 className="text-2xl font-bold text-black">Add GeoNote</h1>
+      <h1 className="text-2xl font-bold text-black dark:text-white">Add GeoNote</h1>
       <AddGeoNoteForm />
     </div>
   );
