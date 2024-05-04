eb2b6d5c589f1b19a319f5e31acb1fcb18520854
LuggaPugga <LuggaPugga>
2024-05-04 15:41:04 +0200

    fixed invalid DOM property

--- app/_components/icons/LightIcon.jsx
+++ app/_components/icons/LightIcon.jsx
@@ -2,7 +2,7 @@ export default function LightIcon({ className }) {
   return (
     <div className="group">
       <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none"
-           stroke="currentColor" stroke-width="2" stroke-linecap="round" strokeLinejoin="round"
+           stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"
            className="lucide lucide-sun">
         <circle cx="12" cy="12" r="4"/>
         <path d="M12 2v2"/>
--- app/_components/icons/SystemIcon.jsx
+++ app/_components/icons/SystemIcon.jsx
@@ -2,7 +2,7 @@ export default function SystemIcon({ className }) {
   return (
     <div className="group">
       <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none"
-           stroke="currentColor" stroke-width="2" stroke-linecap="round" strokeLinejoin="round"
+           stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"
            className="lucide lucide-sun-moon">
         <path d="M12 8a2.83 2.83 0 0 0 4 4 4 4 0 1 1-4-4"/>
         <path d="M12 2v2"/>
